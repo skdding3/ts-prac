58dfe35a944cf4a0ed164d44cb528f491072085d
skdding3 <skdding3@gmail.com>
2022-05-30 16:41:51 +0900

    TypeScript Create

---
+++ First Type Annotation.ts
@@ -0,0 +1,15 @@
+// First Type Annotation
+
+let a: string;
+
+a = "Sunki";
+
+a = 31;
+
+// 첫번째 타입 선언에 따라 할당 조건을 다르게 지정할 수 있다.
+
+function hello(b: number) {}
+
+hello("Sunki");
+
+// b가 넘버로 선언되어서 매개 변수 할당이 성립하지 않는다.
