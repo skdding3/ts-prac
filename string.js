cb9d5e3ed7d275fc669751bf586455032d1d2d40
skdding3 <skdding3@gmail.com>
2022-06-03 10:57:40 +0900

    Number and String

---
+++ string.js
@@ -0,0 +1,10 @@
+"use strict";
+let myName = "sunki";
+myName = "sunki";
+// Template String by ES6
+let fullName = "Sunki Kim";
+let age = 31;
+let sentence = `Hello, My name is ${fullName}.
+
+i'll be ${age + 1} years old next month`;
+console.log(sentence);
---
+++ string.ts
@@ -0,0 +1,14 @@
+let myName: string = "sunki";
+
+myName = "sunki";
+
+// Template String by ES6
+
+let fullName: string = "Sunki Kim";
+let age: number = 31;
+
+let sentence: string = `Hello, My name is ${fullName}.
+
+i'll be ${age + 1} years old next month`;
+
+console.log(sentence);
