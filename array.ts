f25c93f3091fdfb9a47bcc94c3ab7809a6b2fff3
skdding3 <skdding3@gmail.com>
2022-06-05 20:13:44 +0900

    unknown

---
+++ array.ts
@@ -0,0 +1,3 @@
+let list: (number | string)[] = [1, 2, 3, "4"];
+
+// let list: Array<Number> = [1, 2, 3];
---
+++ unknown.ts
@@ -0,0 +1,17 @@
+// unknown
+
+declare const maybe: unknown;
+
+// const aNumber: number = maybe;
+
+if (maybe === true) {
+  const aBoolean: boolean = maybe;
+
+  // const aString: string = maybe;
+}
+
+if (typeof maybe === "string") {
+  const aString: string = maybe;
+
+  // const aBoolean: boolean = maybe;
+}
