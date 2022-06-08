137595c7041c1a3c1e59715e55cdd0508c46b707
skdding3 <skdding3@gmail.com>
2022-06-08 12:14:01 +0900

    never

--- never.ts
+++ never.ts
@@ -11,3 +11,12 @@ function fail() {
 function infiniteLoop() : never{
   while (true) {}
 }
+
+
+let a: string = 'hello'
+
+if (typeof a !== 'string') {
+	let b: never = a;
+}
+
+type Indexable<T> = T extends string ? T & { [index: string]: any } : never;
